fd9076afb4be828fa60564d09a31d1dff88f2a4c
Samhitha Poladi <samhithapoladi@gmail.com>
2026-03-29 22:40:52 +0530

    Updated game.py with a function for checking for draws

--- game.py
+++ game.py
@@ -3,15 +3,16 @@ from pygame.locals import *
 import numpy as np
 pygame.init()
 class games:
-    def __init__(self,active = 1,n_active = 2, board = np.zeros(5)):
-        self.active = active
-        self.n_active = n_active
+    def __init__(self, player1, player2, board = np.zeros(5)):
+        self.active = 1
+        self.n_active = 2
         self.board = board
+        self.winner = None
     def switch(self):
-        x = self.active
-        self.active = self.n_active
-        self.n_active = x
+        self.active, self.n_active = self.n_active, self.active
     def win_check(self):
         pass
     def display(self):
         print(self.board)
+    def is_board_full(self):
+        return not np.any(self.board==0)
