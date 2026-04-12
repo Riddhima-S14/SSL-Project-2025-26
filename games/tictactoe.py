51cb8e755e1f2f22d3332044ffb3571727073711
Riddhima-S14 <thatsenoughletsgohome14@gmail.com>
2026-04-12 15:12:38 +0530

    Updated tictactoe.py
    Updated tictactoe so that it is a class that can be called in game.py
    Removed loops from the win condition

--- games/tictactoe.py
+++ games/tictactoe.py
@@ -1,13 +1,12 @@
 from game import games
 import numpy as np
+import time
 import pygame
 from pygame.locals import *
 pygame.init()
 
 #player 1 - circle - 1
 #player 2 - cross - 2 
-display = pygame.display.set_mode((900,750))
-board = np.zeros(100).reshape((10,10))
 #colors
 
 BLUE  = (0, 0, 255)
@@ -15,26 +14,6 @@ RED   = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
- 
-FPS = pygame.time.Clock()
-FPS.tick(60)
-
-def end_message(text, seconds):
-    display.fill((251,72,196)) 
-    change_font = pygame.font.SysFont("Times New Roman", 60,bold=True)
-    message_surface = change_font.render(text, True, (0, 0, 0)) 
-    #pygame.draw.rect(display, (0, 0, 0), (375, 375, , 50))
-    rect = message_surface.get_rect(center=(450, 375))
-    display.blit(message_surface, rect)
-    
-    pygame.display.flip() 
-    time.sleep(seconds)     
-#drawing
-def draw_cross(surface, color, center_pos, size):
-    x, y = center_pos
-    pygame.draw.line(surface, color, (x - size+5, y - size), (x + size-5, y + size),5)
-    pygame.draw.line(surface, color, (x + size-5, y - size), (x - size+5, y + size),5)
-
 
 class Tictactoe(games):
     def occ(self,x,y):
@@ -52,73 +31,92 @@ class Tictactoe(games):
     def win_check(self):
         rows, cols = self.board.shape
         length = 5
-
-        for r in range(rows):
-            for c in range(cols-length+1):
-                slice=self.board[r, c:c+length]
-                if np.all(slice==self.active):
-                    return self.active
-        
-        for c in range(cols):
-            for r in range(rows-length+1):
-                slice=self.board[r:r+length, c]
-                if np.all(slice==self.active):
-                    return self.active
-                
-        for r in range(rows-length+1):
-            for c in range(cols-length+1):
-                slice=self.board[r:r+length, c:c+length]
-                if np.all(np.diagonal(slice)==self.active):
-                    return self.active
-        
-        for r in range(rows-length+1):
-            for c in range(rows-length+1):
-                slice=self.board[r:r+length, c:c+length]
-                flipped=np.diagonal(np.fliplr(slice))
-                if np.all(flipped==self.active):
-                    return self.active
-        
-        return 0
-      
-            
-pygame.display.set_caption("Tic Tac Toe")
-
-play = Tictactoe(1,2,board)
-game_on=True
-
-while game_on:
-    for event in pygame.event.get(): 
-        if event.type == QUIT:
-            pygame.quit()
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            position = event.pos
-            g_box = play.box(position)
-            if (g_box[0] >= 0):
-                if (play.occ(g_box[0],g_box[1]) == False):
-                    play.board[g_box[0],g_box[1]] = play.active
-                    winner=play.win_check()                     
-                    if winner!=0:
-                        play.winner=play.active
-                        text = f"Player {play.active} Wins!"
-                        end_message(text,2)
-                        game_on=False
-                    elif not np.any(play.board == 0):
+        t = self.board == self.active #truth matrix
+        #we use binary logic to check 
+        #for horizontal
+        h = t[:,0:0+length+1] & t[:,1:2+length] & t[:,2:3+length] & t[:,3:length+4] & t[:,4:length+5]
+        if np.any(h):
+            posn = np.where(h == 1)
+            self.win_line = ((135+posn[1][0]*70,60+posn[0][0]*70),(4*70+135+posn[1][0]*70,60+posn[0][0]*70))
+            return self.active
+        #for vertical
+        v = t[0:0+length+1,:] & t[1:2+length,:] & t[2:3+length,:] & t[3:length+4,:] & t[4:length+5,:]
+        if np.any(v):
+            posn = np.where(v ==1) 
+            self.win_line = ((135+int(posn[1][0])*70,60+int(posn[0][0])*70),(135+int(posn[1][0])*70,4*70+60+int(posn[0][0])*70))        
+            return self.active
+        #top left to bottom right diagonal
+        d1 = t[0:0+length+1,0:0+length+1] &   t[1:2+length,1:2+length] & t[2:3+length,2:3+length] & t[3:length+4,3:length+4] & t[4:length+5,4:length+5]
+        if np.any(d1):
+            posn = np.where(d1 == 1)
+            self.win_line = ((135+posn[1][0]*70,60+posn[0][0]*70),(4*70+135+posn[1][0]*70,4*70+60+posn[0][0]*70))
+            return self.active
+        #bottom left to top right
+        d2 = t[rows-1:rows-1-length-1,0:0+length+1] & t[rows-2:rows-length-3,1:2+length] & t[rows-3:rows-length-4,2:3+length] & t[rows-4:rows-length-5,4:length+5]
+        if np.any(d2):
+            posn = np.where(d2 == 1)
+            self.win_line = ((135+(posn[1][0])*70,60+(posn[0][0]+4)*70),(4*70+135+posn[1][0]*70,60+(posn[0][0]+4)*70-4*70))
+            return self.active
+        return 0 
+    def draw_cross(self,surface, color, center_pos, size):
+        x, y = center_pos
+        pygame.draw.line(surface, color, (x - size+5, y - size), (x + size-5, y + size),5)
+        pygame.draw.line(surface, color, (x + size-5, y - size), (x - size+5, y + size),5)
+    def execution(self):
+        FPS = pygame.time.Clock()
+        FPS.tick(60)
+        display = pygame.display.set_mode((900,750))
+        self.board = np.zeros(100).reshape((10,10))
+        pygame.display.set_caption("Tic Tac Toe")
+        game_on=True
+        while True:
+            for event in pygame.event.get(): 
+                if event.type == QUIT:
+                    pygame.quit()
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    position = event.pos
+                    g_box = self.box(position)
+                    if (g_box[0] >= 0):
+                        if (self.occ(g_box[0],g_box[1]) == False):
+                            self.board[g_box[0],g_box[1]] = self.active
+                            winner=self.win_check()                     
+                            if winner!=0:
+                                self.winner=self.active
+                                end_time=time.time()
+                                game_on=False
+                            elif not np.any(self.board == 0):
                         #draw
-                        end_message("Its a Draw!",2)
-                        game_on=False
-                    else:
-                        play.switch()
+                                end_time=time.time()
+                                game_on=False
+                            else:
+                                self.switch()
+                elif not game_on and event.type == pygame.KEYDOWN:
+                    pygame.quit()
+                    exit()
 
-    display.fill(BLACK)
-    for i in range(1,10):
-        pygame.draw.line(display,GREEN,(100,25+70*i),(800,25+70*i),3)
-        pygame.draw.line(display,GREEN,(100+70*i,25), (100+70*i,725),3)
-    for i in range(10):
-        for j in range(10):
-            if play.board[j,i] == 1:
-                pygame.draw.circle(display, BLUE, (135+70*i,60+70*j), 25)
-                pygame.draw.circle(display, BLACK, (135+70*i,60+70*j), 20)
-            elif play.board[j,i] == 2:
-                draw_cross(display,RED,(135+70*i,60+70*j),25)
-    
-    pygame.display.update()
+            display.fill(BLACK)
+            for i in range(1,10):
+                pygame.draw.line(display,GREEN,(100,25+70*i),(800,25+70*i),3)
+                pygame.draw.line(display,GREEN,(100+70*i,25), (100+70*i,725),3)
+            for i in range(10):
+                for j in range(10):
+                    if self.board[j,i] == 1:
+                        pygame.draw.circle(display, BLUE, (135+70*i,60+70*j), 25)
+                        pygame.draw.circle(display, BLACK, (135+70*i,60+70*j), 20)
+
+                    elif self.board[j,i] == 2:
+                        self.draw_cross(display,RED,(135+70*i,60+70*j),25)
+            if not game_on and self.win_line:
+                pygame.draw.line(display, WHITE, self.win_line[0], self.win_line[1], 8)
+            if not game_on and time.time()-end_time>1.25:
+                font = pygame.font.SysFont(None, 72)
+                if self.winner != 0:
+                    text = font.render(f"Player {self.winner} Wins!", True, GREEN)
+                else:
+                    text = font.render("It's a Draw!", True, GREEN)
+                rect = text.get_rect(center=(450, 375))
+                pygame.draw.rect(display, BLACK, rect.inflate(40, 40))
+                pygame.draw.rect(display, GREEN, rect.inflate(40, 40), 5)
+                display.blit(text, rect)
+            pygame.display.update()    
+            
